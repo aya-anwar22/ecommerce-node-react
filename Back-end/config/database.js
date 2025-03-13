83df725b4509819564f7dee2cd0f49b787a61cfc
aya-anwar22 <aya786930@gmail.com>
2025-03-13 20:01:12 +0200

    vercel

--- Back-end/config/database.js
+++ Back-end/config/database.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "postgres",
-    port:  5432,
+    dialect: "mariadb",
+    port: process.env.DB_PORT || 3306,
   }
 );
 
