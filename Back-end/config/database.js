a9b156fe66aa6b3d6cebe7adae576dd74402deeb
aya-anwar22 <aya786930@gmail.com>
2025-03-13 19:49:46 +0200

    vercel

--- Back-end/config/database.js
+++ Back-end/config/database.js
@@ -1,21 +1,13 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config({ path: '.env' }); 
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "sqlite",
-    storage: "database.sqlite" ,
-    port:  15736,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-    
+    dialect: "postgres",
+    port:  5432,
   }
 );
 
