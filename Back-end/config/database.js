96217548d5994fda0db2eac489aabe8f8fd79dda
aya-anwar22 <aya786930@gmail.com>
2025-06-27 18:40:46 +0300

    Fix: clean install for Vercel

--- Back-end/config/database.js
+++ Back-end/config/database.js
@@ -1,14 +1,30 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config({ path: './.env' });
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     port: process.env.DB_PORT || 15736,
+//     dialectOptions: {},
+//   }
+// );
+
+// module.exports = sequelize;
 const { Sequelize } = require("sequelize");
-require("dotenv").config({ path: './.env' });
+require("dotenv").config();
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT || 15736,
-    dialectOptions: {},
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres", // استخدمي postgres
+    logging: false,
   }
 );
 
